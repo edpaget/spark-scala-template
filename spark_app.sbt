0bb54d44352afade3be2e9f5e6c8390b2bdcf67e
Edward Paget <edward@cassetteta.pe>
2015-09-17 12:14:54 -0500

    Initial Commit

---
+++ spark_app.sbt
@@ -0,0 +1,8 @@
+name := "Spark App"
+
+version := "1.0"
+
+scalaVersion := "2.10.4"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.5.0"
+libraryDependencies += "com.databricks" %% "spark-avro" % "2.0.1"
---
+++ src/main/scala/SparkApp.scala
@@ -0,0 +1,10 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object SimpleApp {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Simple Application")
+    val sc = new SparkContext(conf)
+  }
+}
